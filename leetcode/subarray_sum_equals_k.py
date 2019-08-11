f871286101eb14da5101593c1b9df893da895004
Tom Viner <tom@viner.tv>
2019-08-12 00:51:56 +0100

    add more example implementations

--- leetcode/subarray_sum_equals_k.py
+++ leetcode/subarray_sum_equals_k.py
@@ -1,4 +1,8 @@
+"""
+https://leetcode.com/problems/subarray-sum-equals-k/
+"""
 from collections import defaultdict
+from itertools import combinations
 from typing import List
 
 
@@ -16,8 +20,43 @@ class Solution:
             # count sum up to and including num
             cum_sum += num
 
-            # `cum_sum - k` is the cumulative sum of the start of all
-            # subarrays that end here, and sum to k. Add them to the count.
+            # `cum_sum - k` is the cumulative sum belonging to the start of all
+            # subarrays that end here, and sum to k. Add the number of them to
+            # the count.
             count += sums[cum_sum - k]
 
         return count
+
+    def tomh_base(self, l, target):
+        combs = list(combinations(range(len(l) + 1), 2))
+        return len([
+            0 for a, b in combs
+            if sum(l[a:b]) == target
+        ])
+
+    def tomh(self, l, target):
+        c, acc, count = {0: 1}, 0, 0
+        for x in l:
+            acc += x
+            c[acc] = c.get(acc, 0) + 1
+            count += c.get(acc - target, 0)
+        return count
+
+    # 3rd party solution
+    # https://leetcode.com/problems/subarray-sum-equals-k/discuss/348714/Python-Subarray-Sum-Equals-K
+    def klick(self, nums, k):
+        n = len(nums)
+        dic, _sum, counter = {0: 1}, 0, 0
+
+        for i in range(0, n):
+            _sum += nums[i]
+
+            if _sum - k in dic:
+                counter += dic[_sum - k]
+
+            if _sum in dic:
+                dic[_sum] += 1
+            else:
+                dic[_sum] = 1
+
+        return counter
--- leetcode/test_subarray_sum_equals_k.py
+++ leetcode/test_subarray_sum_equals_k.py
@@ -1,15 +1,58 @@
 import pytest
 
+from hypothesis import given
+from hypothesis.strategies import lists, integers, composite
+
 from subarray_sum_equals_k import Solution
 
 
-@pytest.mark.parametrize('nums, k, count', [
+param_testcases = pytest.mark.parametrize('nums, k, count', [
+    ([], 0, 0),
+    ([], 1, 0),
+    ([0], 0, 1),
+    ([7], 7, 1),
     ([1, 1, 1], 2, 2),
     ([3, 4, 7, 2, -3, 1, 4, 2], 7, 4),
-    ([0, 1, -1, 0, 2, 3, 1, 1], 2, 5),
     ([0, 1, -1, 0, 2, 3, 1, 1], 0, 6),
+    ([0, 1, -1, 0, 2, 3, 1, 1], 1, 5),
+    ([0, 1, -1, 0, 2, 3, 1, 1], 2, 5),
     ([0, 0, 0], 0, 6),
     ([0, 0, 0], 1, 0),
 ])
+
+
+@param_testcases
 def test_subarray_sum(nums, k, count):
     assert Solution().subarraySum(nums, k) == count
+
+
+@param_testcases
+def test_tomh_base(nums, k, count):
+    assert Solution().tomh_base(nums, k) == count
+
+
+@param_testcases
+def test_tomh(nums, k, count):
+    assert Solution().tomh(nums, k) == count
+
+
+@param_testcases
+def test_klick(nums, k, count):
+    assert Solution().klick(nums, k) == count
+
+
+@composite
+def nums_and_k(draw):
+    max_num = draw(integers(min_value=1, max_value=10))
+    nums = draw(lists(integers(min_value=-max_num, max_value=max_num)))
+    k = draw(integers(min_value=-max_num, max_value=max_num))
+    return nums, k
+
+
+@given(nums_and_k())
+def test_all_equivalent(nums_k):
+    nums, k = nums_k
+    s = Solution()
+    base_count = s.tomh_base(nums, k)
+    for method in (s.subarraySum, s.tomh, s.klick):
+        assert method(nums, k) == base_count
