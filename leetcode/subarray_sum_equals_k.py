bc6c5013367411e805729f562db736254bccb069
Tom Viner <tom@viner.tv>
2019-08-13 23:03:01 +0100

    improve var name

--- leetcode/subarray_sum_equals_k.py
+++ leetcode/subarray_sum_equals_k.py
@@ -8,22 +8,23 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = cum_sum = 0
+        count = 0
+        acc = 0
         sums = defaultdict(int)
 
         for num in nums:
             # store the count of cumulative sums up to just before num.
             # think of these as all the possible starting positions, which
             # is why this never sees the final num.
-            sums[cum_sum] += 1
+            sums[acc] += 1
 
             # count sum up to and including num
-            cum_sum += num
+            acc += num
 
-            # `cum_sum - k` is the cumulative sum belonging to the start of all
+            # `acc - k` is the cumulative sum belonging to the start of all
             # subarrays that end here, and sum to k. Add the number of them to
             # the count.
-            count += sums[cum_sum - k]
+            count += sums[acc - k]
 
         return count
 
