776253cc646b55c5546c4fab60df46d9a85ff6ee
Tom Viner <tom@viner.tv>
2020-06-24 20:57:36 +0100

    move minesweeping to own repo
    https://github.com/tomviner/minesweeper

--- projects/minesweeper/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders cells', () => {
-  const { getAllByText } = render(<App />);
-  const cells = getAllByText('go');
-  cells.forEach(cell => expect(cell).toBeInTheDocument());
-});
