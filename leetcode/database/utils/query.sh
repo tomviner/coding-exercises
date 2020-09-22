60ec4d7bedda1ea83a47702787fa87bff961fe80
Tom Viner <tom@viner.tv>
2020-09-23 00:24:09 +0100

    improve query script

--- leetcode/database/utils/query.sh
+++ leetcode/database/utils/query.sh
@@ -1,5 +1,8 @@
+SUFFIX=${1:-}
+
 for db in $(ls data*.db);
     do
-        /home/tom/bin/sqlite3 $db -column -header < query.sql
+        echo $db
+        /home/tom/bin/sqlite3 $db -column -header < query${SUFFIX}.sql
         echo
 done
